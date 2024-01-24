6546698812f8bfef5ea6fceb5a9b570c2301ac2e
tooma81 <toomas.kaarel@gmail.com>
2024-01-24 14:01:50 +0200

    Added dynamic values

--- src/App.js
+++ src/App.js
@@ -2,9 +2,26 @@ import './App.css';
 import ExpenseItem from './components/ExpenseItem';
 
 function App() {
+  const expenses = [ 
+  {   
+    date: new Date(2023, 9, 6),
+    title: 'New Book',
+    price: 30.99
+  }, 
+  {   
+    date: new Date(2023, 9, 6),
+    title: 'New Jeans',
+    price: 99.99
+  }, 
+  ] 
   return (
     <div className='App'>
-      <ExpenseItem />
+      <ExpenseItem 
+        expenseData = {expenses[0]}   
+      />
+      <ExpenseItem 
+        expenseData = {expenses[1]}   
+      />
     </div>
   );
 }
