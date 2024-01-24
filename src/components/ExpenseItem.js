d40faba2d3ae8ad9f532124dbbbf0fc2510768e4
tooma81 <toomas.kaarel@gmail.com>
2024-01-24 14:15:28 +0200

    Updated date format

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,18 @@
 import './ExpenseItem.css'
 
 function ExpenseItem(props) { 
-    console.log(props)
+    const day = props.expenseData.date.toLocaleString
+        ('en-US', {day: '2-digit'})
+    const month = props.expenseData.date.toLocaleString
+        ('en-US', {month: 'long'})
+    const year = props.expenseData.date.getFullYear()
     return (
         <div className="expense-item">
-            <div>{props.expenseData.date.toString()}</div>
+            <div>
+                <div>{month}</div>
+                <div>{day}</div>
+                <div>{year}</div>
+            </div>
             <div className='expense-item__desc'>
                 <h2>{props.expenseData.title}</h2>
                 <div className='expense-item__price'>
