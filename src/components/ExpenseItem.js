475f4a3aef817e23ffd3770038eba8dc1b6f1d1b
tooma81 <toomas.kaarel@gmail.com>
2024-01-25 09:33:05 +0200

    Added separate Date file

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,18 +1,12 @@
 import './ExpenseItem.css'
+import ExpenseDate from './ExpenseDate'
 
 function ExpenseItem(props) { 
-    const day = props.expenseData.date.toLocaleString
-        ('en-US', {day: '2-digit'})
-    const month = props.expenseData.date.toLocaleString
-        ('en-US', {month: 'long'})
-    const year = props.expenseData.date.getFullYear()
     return (
         <div className="expense-item">
-            <div>
-                <div>{month}</div>
-                <div>{day}</div>
-                <div>{year}</div>
-            </div>
+            <ExpenseDate
+                date = {props.expenseData.date}   
+            />
             <div className='expense-item__desc'>
                 <h2>{props.expenseData.title}</h2>
                 <div className='expense-item__price'>
