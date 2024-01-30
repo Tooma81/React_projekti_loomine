39609c68748f6eb629bd436f9a71915e1051880b
tooma81 <toomas.kaarel@gmail.com>
2024-01-30 14:05:21 +0200

    Reorganized component files into folders

---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,14 @@
+import './Expenses.css'
+import Card from '../UI/Card'
+import ExpenseItem from './ExpenseItem'
+
+function Expenses(props){
+    return(
+        <Card className='expenses'>
+            <ExpenseItem expenseData={props.expenses[0]} />
+            <ExpenseItem expenseData={props.expenses[1]} />
+        </Card>
+    )
+}  
+
+export default Expenses
